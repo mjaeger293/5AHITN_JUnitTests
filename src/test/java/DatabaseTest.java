39ddc4a48cf50ddf98ac748b0d3b4e635b54ca39
mjaeger293 <mjaeger@htl-steyr.ac.at>
2022-10-05 09:40:49 +0200

    Divers

--- src/test/java/DatabaseTest.java
+++ src/test/java/DatabaseTest.java
@@ -10,6 +10,6 @@ class DatabaseTest {
     void createTable() throws SQLException {
         Connection connection = MySQLInstance.getInstance();
 
-        connection.prepareStatement("CREATE TABLE fraction (dividend INT, divisor INT)").executeQuery();
+        connection.prepareStatement("CREATE TABLE fraction (dividend INT, divisor INT)").executeUpdate();
     }
 }
