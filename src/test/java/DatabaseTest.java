35a8ce1beccaadacff4c88f1c2152dc0e1a7e284
mjaeger293 <mjaeger@htl-steyr.ac.at>
2022-11-09 08:48:57 +0100

    Changes from Rathgeb

--- src/test/java/DatabaseTest.java
+++ src/test/java/DatabaseTest.java
@@ -1,7 +1,10 @@
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.function.ThrowingSupplier;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.Statement;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class DatabaseTest {
@@ -13,35 +16,86 @@ class DatabaseTest {
         db = MySQLInstance.getInstance();
     }
 
+    private Connection connect(String db) {
+        Connection conn = null;
+
+        try {
+            if (db.length() > 0) {
+                conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/" + db, "root", "root");
+            } else {
+                conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306", "root", "root");
+            }
+        } catch(Exception e) {
+
+        }
+
+        return conn;
+    }
+
     @Test
     @Order(1)
-    void createDatabase() {
-        Assertions.assertDoesNotThrow(() -> {
+    void connectToDatabase() {
+        /*Assertions.assertDoesNotThrow(() -> {
             db.prepareStatement("CREATE DATABASE `junit-test`").executeUpdate();
             db.prepareStatement("USE `junit-test`").execute();
+        });*/
+
+        Assertions.assertDoesNotThrow(new ThrowingSupplier<Connection>() {
+            @Override
+            public Connection get() throws Throwable {
+                return connect("");
+            }
         });
     }
 
     @Test
     @Order(2)
-    void createTable() {
+    void createDatabase() {
+        /*Assertions.assertDoesNotThrow(() -> {
+            db.prepareStatement("CREATE DATABASE `junit-test`").executeUpdate();
+            db.prepareStatement("USE `junit-test`").execute();
+        });*/
+
         Assertions.assertDoesNotThrow(() -> {
-            db.prepareStatement("CREATE TABLE fraction (dividend INT, divisor INT)").executeUpdate();
+            Connection c = connect("");
+
+            Statement s = c.createStatement();
+
+            s.executeUpdate("CREATE DATABASE testdb");
+
+            s.execute("USE `testdb`");
+
+            s.close();
+            c.close();
         });
     }
 
     @Test
     @Order(3)
-    void insert() {
+    void createTable() {
         Assertions.assertDoesNotThrow(() -> {
-            db.prepareStatement("INSERT INTO fraction (dividend, divisor) VALUES (10, 5)").executeUpdate();
+            Connection c = connect("testdb");
+
+            c.prepareStatement("CREATE TABLE fraction (dividend INT, divisor INT)").executeUpdate();
         });
     }
 
     @Test
     @Order(4)
+    void insert() {
+        Assertions.assertDoesNotThrow(() -> {
+            Connection c = connect("testdb");
+
+            c.prepareStatement("INSERT INTO fraction (dividend, divisor) VALUES (10, 5)").executeUpdate();
+        });
+    }
+
+    @Test
+    @Order(5)
     void select() {
         Assertions.assertDoesNotThrow(() -> {
+            Connection c = connect("testdb");
+
             ResultSet result = db.prepareStatement("SELECT * FROM fraction LIMIT 1").executeQuery();
 
             if (result.next()) {
